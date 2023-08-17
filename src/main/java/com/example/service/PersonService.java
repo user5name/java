45dd9527df909e9881985f5cda02fb3b3df0695b
user5name <101582218+user5name@users.noreply.github.com>
2023-08-17 16:29:59 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -19,39 +19,7 @@ public class PersonService {
 		p.setLastName("te");
 		persons.add(p);
 
-		Person p = new Person();
-		p.setId("182");
-		p.setAge(12);
-		p.setFirstName("te");
-		p.setLastName("tesst");
-		persons.add(p);
-		
-		p = new Person();
-		p.setId("2");
-		p.setAge(25);
-		p.setFirstName("Mohammed");
-		p.setLastName("Mohammed");
-		persons.add(p);
-		
-		p = new Person();
-		p.setId("3");
-		p.setAge(25);
-		p.setFirstName("Salma");
-		p.setLastName("Salma");
-		persons.add(p);
-		
-		p = new Person();
-		p.setId("4");
-		p.setAge(25);
-		p.setFirstName("test");
-		p.setLastName("test");
-		persons.add(p);
-		p = new Person();
-		p.setId("48");
-		p.setAge(21);
-		p.setFirstName("test09");
-		p.setLastName("test09");
-		persons.add(p);
+	
 		
 	}
 	public Person getPerson(String id) {
